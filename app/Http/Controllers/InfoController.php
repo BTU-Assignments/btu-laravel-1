2876d170caa2b3ad0ae981e22d1bc1cc250d1d87
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-10-14 20:43:51 +0400

    finished

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -6,5 +6,66 @@ use Illuminate\Http\Request;
 
 class InfoController extends Controller
 {
-    //
+    public function index()
+   {
+        $info = [
+            'name' => 'Giorgi',
+            'surname' => 'Kalatozishvili',
+            'age' => 20,
+            'hobby' => 'Software Developer',
+        ];
+
+        return response()->json($info);
+    }
+
+    public function whoami()
+    {
+         $info = [
+            'full_name' => 'Giorgi Kalatozishvili',
+        ];
+
+        return response()->json($info);
+    }
+
+
+    public function age()
+    {
+         $info = [
+            'age' => '20',
+        ];
+
+        return response()->json($info);
+    }
+    public function hobby()
+    {
+         $info = [
+            'hobby' => 'Coding and Sleeping',
+        ];
+
+        return response()->json($info);
+    }
+
+    public function funfact()
+    {
+         $info = [
+            'funfact' => 'I love lobio',
+        ];
+
+        return response()->json($info);
+    }
+
+    public function create()
+    {
+         return response()->json(['message' => 'წარმატებით დაემატა']);
+    }
+
+    public function update()
+    {
+         return response()->json(['message' => 'წარმატებით განახლდა']);
+    }
+
+    public function destroy()
+    {
+        return response()->json(['message' => 'წარმატებით წაიშალა']);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InfoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// GET Routes
+Route::get('/info', [InfoController::class, 'index']);
+Route::get('/whoami', [InfoController::class, 'whoami']);
+Route::get('/age', [InfoController::class, 'age']);
+Route::get('/hobby', [InfoController::class, 'hobby']);
+Route::get('/funfact', [InfoController::class, 'funfact']);
+
+// POST Route
+Route::post('/info', [InfoController::class, 'create']);
+
+// PUT Route
+Route::put('/info', [InfoController::class, 'update']);
+
+// DELETE Route
+Route::delete('/info', [InfoController::class, 'destroy']);
+
